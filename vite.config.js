16ab840cba7ede50bc076e122b90e1abeb0a1d71
Arishah Khan <arishahsanaullah78@gmail.com>
2025-04-27 15:56:32 +0500

    Changes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.NODE_ENV === 'development' 
+          ? 'http://localhost:5000'  
+          : 'https://mernstackbackend-production-86ac.up.railway.app', 
         changeOrigin: true,
         secure: false,
       }
