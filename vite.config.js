bee76fb3685eb9fb8993b5a70cd5febc4950474d
Arishah Khan <arishahsanaullah78@gmail.com>
2025-04-27 16:18:05 +0500

    Changes

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',  // Backend ka URL
-        changeOrigin: true,              // Origin ko change karne ke liye
-        secure: false,                   // Agar backend HTTP pe ho toh
-      }
-    }
-  }
+  plugins: [react() ,tailwindcss()
+  ],
 })
